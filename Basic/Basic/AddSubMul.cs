6e020dc29fb21450b3e99cd85410bd87601f8e42
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-02 17:56:49 +0530

    [Rishi]Adding:AddMulSub By User

---
+++ Basic/Basic/AddSubMul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic
+{
+    class AddSubMul
+    {
+        int num1;
+        int num2;
+        public void Add()
+        {
+            Console.WriteLine("give input to num1");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Give input to num2");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Adding two numbers" + num1 + num2);
+            Console.WriteLine("Subracting Two Numbers"+   num1,-num2);
+            Console.WriteLine("Multiplication Two Numbers" + num1 * num2);
+            Console.WriteLine("Dividing Two numbers" + num1 / num2);
+
+        }
+    }
+}
--- Basic/Basic/Program.cs
+++ Basic/Basic/Program.cs
@@ -6,20 +6,52 @@ namespace Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Basic programs");
-            basic OBj = new basic();
-            OBj.Name();
-            Adding add = new Adding();
-            add.sum();
-            Division div = new Division();
-            div.Div();
-            Arthamatic art = new Arthamatic();
-            art.add();
-            Swap swap = new Swap();
-            swap.main();
-            Multiplication bj = new Multiplication();
-            bj.Mul();
 
+            Console.WriteLine("press 0 to Name");
+            Console.WriteLine("Press 1 to Addition");
+            Console.WriteLine("Press 2 to division");
+            Console.WriteLine("Press 3 to Arthamatic");
+            Console.WriteLine("Press 4 to Swaping");
+            Console.WriteLine("Press 5 to Multilication");
+            Console.WriteLine("Press 6 to AddMulSub by user");
+            int selectio = Convert.ToInt32(Console.ReadLine());
+            switch (selectio)
+            {
+
+                case 0: 
+                  basic OBj = new basic();
+                  OBj.Name();
+                  break;
+                case 1:
+                  Adding add = new Adding();
+                  add.sum();
+                  break;
+                case 2:
+                  Division div = new Division();
+                  div.Div();
+                  break;
+                case 3:
+                  Arthamatic art = new Arthamatic();
+                  art.add();
+                  break;
+                case 4:
+                  Swap swap = new Swap();
+                  swap.main();
+                    break;
+                case 5:
+                  Multiplication bj = new Multiplication();
+                  bj.Mul();
+                    break;
+                case 6:
+                    AddSubMul Obj = new AddSubMul();
+                    Obj.Add();
+                    break;
+                default:
+                    Console.WriteLine("in valid expresion");
+                    break;
+
+
+        }
         }
     }
 }
