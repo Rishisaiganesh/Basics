1b7233988a05943fcb9be04a5bf03de06166d843
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-02 16:11:41 +0530

    [Rishi]Adding:name

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic programs");
+            basic OBj = new basic();
+            OBj.Name();
+
+        }
+    }
+}
---
+++ Basic/Basic/basic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic
+{
+    class basic
+    {
+        public void Name()
+        {
+            Console.WriteLine("Rishi");
+
+        }
+    }
+}
